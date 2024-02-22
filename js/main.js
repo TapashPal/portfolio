872ac9d5cefb1ecd6900cc8b44cbbf03c993076c
Tapash Pal <34719588+TapashPal@users.noreply.github.com>
2024-02-22 23:16:00 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@
     .add({
         targets: '#loader',
         opacity: 0,
-        duration: 100,
+        duration: 10,
         begin: function(anim) {
             window.scrollTo(0, 0);
         }
